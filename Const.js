7f9628ff7e568d0c2bd6163f7025c9fa2f131b88
heimao123-abc <68840065+heimao123-abc@users.noreply.github.com>
2020-07-27 18:58:00 +0530

    Update Const.js

--- Const.js
+++ Const.js
@@ -3,11 +3,11 @@ var Const = {};
 Const.useMlab = false;
 //改成你自己的机器人 Token
 //Live
-// Const.botToken = "机器人ID";
+// Const.botToken = "1297296612";
 //Debug
 Const.botToken = "1297296612";
 Const.myId = parseInt(Const.botToken.split(":")[0]);
-Const.myName = "@k1tten_bot";
+Const.myName = "k1tten_bot";
 Const.myName = Const.myName.toLowerCase();
 
 //以下是峰哥的数据库服务器，可以工作，可以用，没有数据库的可以保留
